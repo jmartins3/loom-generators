88a1b2b9a3ff452dfb93e6bd21d3a806e52077c8
jmartins3 <jmartinsisel.pt>
2024-04-16 11:06:34 +0100

    some generators in action

--- src/main/java/pt/isel/pc/sequences1/SequenceRunnable.java
+++ src/main/java/pt/isel/pc/sequences1/SequenceRunnable.java
@@ -2,7 +2,7 @@ package pt.isel.pc.sequences1;
 
 public abstract class SequenceRunnable<T> implements Runnable {
    
-    private pt.isel.pc.sequences1.Sequence.SequenceIterator<T> currIt;
+    private Sequence.SequenceIterator<T> currIt;
     
     protected SequenceRunnable( ) { }
     
